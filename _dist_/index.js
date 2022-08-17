f40eb13fa3a67e257044a3a922b926272605510c
ERJavier <ERJavier@users.noreply.github.com>
2022-08-17 13:04:57 +0000

    deploy: e68c3214af2e1874ffcdcf52f9aa7b4cdb26e07e

--- _dist_/index.js
+++ _dist_/index.js
@@ -3,4 +3,29 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const url = "https://platzi-avo.vercel.app/api/avo";
+
+
+//web api
+//conectarnos al server
+window
+    const todoLosItems = []
+    .fetch(url)
+//procesar la respuesta y conventirla en JSON
+    .then((respuesta) => respuesta.json())
+// JSON -> Data -> renderizar info browser
+    .then((respuestaJson) => {
+        respuestaJson.data.forEach(item => {
+            //crear imagen
+            const imagen = document.createElement('img')
+            //crear titulo
+            const title = document.createElement('h2')
+            //crear precio
+            const price = document.createElement('div')
+
+            const container = document.createElement('div')
+            container.append(imagen, title, price);
+            todoLosItems.push(container);
+        });
+        document.body.append(...todoLosItems);
+    })
