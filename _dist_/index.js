6e37314584f63141601c2ae9790bb62f1c43f506
ERJavier <ERJavier@users.noreply.github.com>
2022-08-19 01:33:22 +0000

    deploy: 390213fe5464cae617463eb3ef95cd737dc8609f

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,11 +1,18 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
-
 const urlBase = "https://platzi-avo.vercel.app";
 
 const appNode = document.querySelector('#app');
+
+//intl
+//format dates and currency
+
+const formartPrice = (price) => {
+    const newPrice = new window.Intl.NumberFormat('en-EN', {
+        style: 'currency',
+        currency: "USD",
+    }).format(price);
+    return newPrice;
+}
+
 //web api
 //conectarnos al server
 window
@@ -20,12 +27,18 @@ window
             //crear imagen
             const imagen = document.createElement("img");
             imagen.src = `${urlBase}${item.image}`;
+            imagen.className = "w-full h-full object-center object-cover lg:w-full lg:h-full";
+
             //crear titulo
             const title = document.createElement("h2");
             title.textContent = item.name;
+            title.className = "mt-1 text-sm text-gray-500"
+
             //crear precio
             const price = document.createElement("div");
-            price.textContent = item.price;
+            price.textContent = formartPrice(item.price);
+            price.className = "text-sm font-medium text-gray-900s"
+
 
             const container = document.createElement("div");
             container.append(imagen, title, price);
